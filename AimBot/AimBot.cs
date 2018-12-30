dd481513a8337f7c9ab92a5622dac5b29ae76f35
Qiu <a2393868407@sina.com>
2018-12-30 17:09:12 +0800

    修改

--- AimBot/AimBot.cs
+++ AimBot/AimBot.cs
@@ -13,16 +13,19 @@ namespace AimBot
 {
 	public class AimBot
 	{
+		public const float SCALAR = 5f;
+		public const int DIST = 100;
 		public static int _timer = 0;
 		public static bool Enable = false;
-		public static float BulletSpeed = 48f;//初始值
-		public static float Scalar = 0.5f;//初始值
-		public static float Dist = 50 * 16f;
+		public static float BulletSpeed = 0;//初始值
+		public static float Scalar = 4f;//初始值
+		public static int Dist = 100;
 		public static float Dist_Mouse = 128;
 
 		static AimBot()
 		{
 			PHooks.Hooks.Update.Pre += Update_Pre;
+			PHooks.Hooks.Chat.Pre += Chat_Pre;
 			PHooks.Hooks.PlayerItemCheckWrapped.Pre += PlayerItemCheckWrapped_Pre;
 		}
 
@@ -51,8 +54,9 @@ namespace AimBot
 							Main.NewText("需要一个参数");
 							return false;
 						}
+						float sc = Scalar;
 						Scalar = Convert.ToSingle(s[1]);
-						Main.NewText("提前量常数已修改为：" + Scalar);
+						Main.NewText("提前量常数已修改为：" + Scalar + "，原来为" + sc);
 					}
 					else if (s[0] == ".cd")
 					{
@@ -61,7 +65,15 @@ namespace AimBot
 							Main.NewText("需要一个参数");
 							return false;
 						}
-						Dist = Convert.ToSingle(s[1]) * 16f;
+						int d = Dist;
+						Dist = Convert.ToInt32(s[1]);
+						Main.NewText("怪物探索距离已修改为：" + Dist + "个方块距离，原来为:" + d + "个方块距离");
+					}
+					else if (s[0] == ".reset")
+					{
+						Scalar = SCALAR;
+						Dist = DIST;
+						Main.NewText("提前量常数已修改为：" + Scalar);
 						Main.NewText("怪物探索距离已修改为：" + Dist + "个方块距离");
 					}
 					return false;
@@ -95,35 +107,28 @@ namespace AimBot
 				{
 					Vector2 mobV = p.velocity;
 					Vector2 a = Main.LocalPlayer.Center - p.Center;
-					if (mobV.LengthSquared() == 0 || a.LengthSquared() == 0)
-					{
-						Vector2 t = p.Center - Main.LocalPlayer.Center;
-						Vector2 y = Main.LocalPlayer.Center - Main.screenPosition + Dist_Mouse * Vector2.Normalize(t);
-						PlayerInput.MouseX = (int)Math.Round(y.X);
-						PlayerInput.MouseY = (int)Math.Round(y.Y);
-					}
-					else
+					Vector2 g = new Vector2(0, 0);//怪物的运动向量
+					if (mobV.LengthSquared() != 0 && a.LengthSquared() != 0)
 					{
 						float cos_t = Vector2.Dot(mobV, a) / (mobV.Length() * a.Length());
 						float d = a.Length();
-						float k = mobV.Length() / BulletSpeed;//48f是猜测的子弹速度
+						float k = mobV.Length() / BulletSpeed;
 
-						{//开始复杂的计算
-						 //delta  = 4k^2d^2cos_t^2-4d^2(k^2-1)
-							double _delta = 4 * Math.Pow(k, 2) * Math.Pow(d, 2) * Math.Pow(cos_t, 2) - 4 * Math.Pow(d, 2) * (Math.Pow(k, 2) - 1);
-							//negative b  = 2kd*cos_t
-							double _nb = 2 * k * d * cos_t;
-							//2a  = 2(k^2-1)
-							double _2a = 2 * (Math.Pow(k, 2) - 1);
-							float t = (float)((_nb - Math.Sqrt(_delta)) / _2a);//t为子弹要运动的距离，这里的正负号取负，如果取正则计算出的目标坐标在怪物身后
-							float kt = k * t;//kt为怪物的运动距离
-							Vector2 g = Vector2.Normalize(mobV) * kt;//g为怪物的运动向量
-							Vector2 j = p.Center + g - Main.LocalPlayer.Center;//j为子弹的运动向量
-							Vector2 y = Main.LocalPlayer.Center - Main.screenPosition + Dist_Mouse * Vector2.Normalize(j);//将其缩放并应用到玩家坐标上
-							PlayerInput.MouseX = (int)Math.Round(y.X);
-							PlayerInput.MouseY = (int)Math.Round(y.Y);
-						}
+						//开始复杂的计算
+						//delta  = 4k^2d^2cos_t^2-4d^2(k^2-1)
+						double _delta = 4 * Math.Pow(k, 2) * Math.Pow(d, 2) * Math.Pow(cos_t, 2) - 4 * Math.Pow(d, 2) * (Math.Pow(k, 2) - 1);
+						//negative b  = 2kd*cos_t
+						double _nb = 2 * k * d * cos_t;
+						//2a  = 2(k^2-1)
+						double _2a = 2 * (Math.Pow(k, 2) - 1);
+						float t = (float)((_nb - Math.Sqrt(_delta)) / _2a);//t为子弹要运动的距离，这里的正负号取负，如果取正则计算出的目标坐标在怪物身后
+						float kt = k * t;//kt为怪物的运动距离
+						g = Vector2.Normalize(mobV) * kt;//g为怪物的运动向量
 					}
+					Vector2 j = p.Center + g - Main.LocalPlayer.Center;//j为子弹的运动向量
+					Vector2 y = Main.LocalPlayer.Center - Main.screenPosition + Dist_Mouse * Vector2.Normalize(j);//将其缩放并应用到玩家坐标上
+					PlayerInput.MouseX = (int)Math.Round(y.X);
+					PlayerInput.MouseY = (int)Math.Round(y.Y);
 				}
 			}
 			return true;
@@ -140,9 +145,10 @@ namespace AimBot
 		{
 			NPC npc = null;
 			Vector2 v = new Vector2(float.MaxValue, float.MaxValue);
+			float f = Dist * 16f;
 			foreach (var n in Main.npc)
 			{
-				if (Vector2.Distance(n.Center, Main.LocalPlayer.Center) > Dist)
+				if (Vector2.Distance(n.Center, Main.LocalPlayer.Center) > f)
 					continue;
 				Vector2 y = Main.LocalPlayer.Center - n.Center;
 				if (n.active && (v = (v.Length() > y.Length() ? y : v)) == y)
